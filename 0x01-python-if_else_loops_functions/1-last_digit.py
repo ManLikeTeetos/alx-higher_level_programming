ecbcadf446d0650a4002f379384b63a53c9c613a
ManLikeTeetos <tolu.mastermindz@gmail.com>
2022-11-29 12:29:43 +0000

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+a = number[-1]
+if(a > 5):
+	print(f"Last Digit of {number:d} is {a:d} and is greater than 5")
+if(a < 6 and a != 0):
+	print(f"Last Digit of {number:d} is {a:d} and is less than 6 and not 0")
+if(a == 0):
+	print(f"Last Digit of {number:d} is {a:d} and is 0")
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,1 +1,2 @@
 [0-positive_or_negative.py](./0-positive_or_negative.py) : This program will assign a random signed number to the variable number each time it is executed <br/>
+[1-last_digit.py](./1-last_digit.py) : Completing source code <br/>
